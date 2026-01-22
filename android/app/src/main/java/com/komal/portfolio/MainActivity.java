e4182157a9634449868b4e1c0ab72d05928d5ffa
komalafzaal <komalafzaal24@gmail.com>
2026-01-22 19:42:13 +0500

    capcitor setup added

---
+++ android/app/src/main/java/com/komal/portfolio/MainActivity.java
@@ -0,0 +1,5 @@
+package com.komal.portfolio;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.komal.portfolio',
+  appName: 'Komal Portfolio',
+  webDir: 'out',
+};
+
+export default config;
